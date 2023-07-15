2e54e59b244b614db4f4677d8d2536481f20f99e
Leonardo de Paula Fernandes <139494876+leodipaula@users.noreply.github.com>
2023-07-15 02:32:13 -0300

    Create Ex2_16-somar_dois_valores_(modulo).java

---
+++ Ex2_16-somar_dois_valores_(modulo).java
@@ -0,0 +1,7 @@
+//Escreva um método com o nome somar que recebe dois valores lidos (a e b). Esse método deve retornar o valor da soma.
+
+public static int somar(int a, int b) {
+    // Este método deve retornar o resultado da soma a + b
+    int soma = a+b;
+    return soma;
+}
